763c28cee94a0b2376a094c2bdef84a98aefb35b
Cesar Lopez <cesarl@outlook.com>
2019-04-18 16:26:31 -0600

    Adding Repository Model and Inerfaces

---
+++ Weather.Server.Repository.Interfaces/DAL/IWeatherDataAccessLayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather.Server.Repository.Interfaces.DAL
+{
+    interface IWeatherDataAccessLayer
+    {
+        /// <summary>
+        /// Retrieve a Collection of Weather
+        /// </summary>
+        /// <returns></returns>
+        List<Model.Weather> GetAllWeathers();
+
+        /// <summary>
+        /// Add Weather
+        /// </summary>
+        /// <param name="weather"></param>
+        void AddWeather(Model.Weather weather);
+
+        /// <summary>
+        /// Get Weather
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Model.Weather GetWeather(DateTime id);
+
+        /// <summary>
+        /// Update Weather
+        /// </summary>
+        /// <param name="weather"></param>
+        void UpdateWeather(Model.Weather weather);
+
+        /// <summary>
+        /// Delete Weather
+        /// </summary>
+        /// <param name="id"></param>
+        void DeleteWeather(DateTime id);
+
+
+    }
+}
---
+++ Weather.Server.Repository.Model/Weather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Weather.Server.Repository.Model
+{
+    public class Weather
+    {
+        public DateTime DateTime { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Sumary { get; set; }
+    }
+}
